161f4f06bd0b60ecd1277fbdb7bf1d235ae1a843
andrushko <andrushko.oleh@mail.ru>
2014-05-05 21:34:40 +0300

    HomeController

---
+++ MvcApplication8/MvcApplication8/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MvcApplicationAdmins.Controllers
+{
+    public class HomeController : Controller
+    {
+        _Data.UserCrud uc = new _Data.UserCrud();
+
+        public ActionResult Index()
+        {
+            ViewBag.Users = uc.View();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult EditUser(int id, int IdRole, int IdStatus)
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("EditUser")]
+        public ActionResult EditUser(int id, int IdRole, int IdStatus, FormCollection collection)
+        {
+            List<Models.User> t = uc.View();
+            try
+            {
+                uc.ChangeRole(id, IdRole);
+                uc.ChangeStatus(id, IdStatus);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DeleteUser(int id)
+        {
+            List<Models.User> t = uc.View();
+            var del = (from c in t where c.id == id select c).First();
+            return View(del);
+        }
+
+        [HttpPost, ActionName("DeleteUser")]
+        public ActionResult DeleteUser(int id, FormCollection collection)
+        {
+            List<Models.User> t = uc.View();
+            var del = (from c in t where c.id == id select c).First();
+            try
+            {
+                uc.DeleteUser(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
